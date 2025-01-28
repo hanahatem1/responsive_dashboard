1b7100ffb42cfcc4a18a9ccb16de21299fdc984f
hanahatem1 <hanahatemm88@gmail.com>
2025-01-28 21:36:52 +0200

    add assets and fonts

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,32 @@
+class MyAsset {
+  MyAsset._();
+
+  static const String cardRecieve = "assets/images/card-receive.svg";
+
+  static const String category = "assets/images/category-2.svg";
+
+  static const String chart = "assets/images/chart-2.svg";
+
+  static const String convertCard = "assets/images/convert-card.svg";
+
+  static const String frame1 = "assets/images/Frame (1).svg";
+
+  static const String frame2 = "assets/images/Frame (2).svg";
+
+  static const String frame3 = "assets/images/Frame (3).svg";
+
+  static const String graph = "assets/images/graph.svg";
+
+  static const String group = "assets/images/Group 237677.svg";
+
+  static const String layout = "assets/images/logout.svg";
+
+  static const String mask = "assets/images/Mask group.svg";
+
+  static const String money = "assets/images/moneys.svg";
+
+  static const String settings = "assets/images/setting-2.svg";
+
+  static const String wallet = "assets/images/wallet-2.svg";
+
+}
---
+++ lib/views/dash_board_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_project/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_project/widgets/dashboard_deskyop_layout.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+      mobileLayout: (context)=>SizedBox(), 
+      tabletLayout: (context)=>SizedBox(),
+      desktopLayout: (context)=>DashboardDeskyopLayout()
+       ),
+    );
+  }
+}
